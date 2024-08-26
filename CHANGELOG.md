5a81c5dcdc31f01a1596d451ae01466e327569c4
Unity Technologies <@unity>
2024-08-26 00:00:00 +0000

    com.unity.polyspatial.xr@1.3.9 ## [1.3.9] - 2024-08-26

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,8 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.3.9] - 2024-08-26
+
 ## [1.3.1] - 2024-07-09
 
 ## [1.3.0] - 2024-06-26
--- Runtime/Input/InputSystemUtility.cs
+++ Runtime/Input/InputSystemUtility.cs
@@ -4,8 +4,17 @@ using UnityEngine.InputSystem;
 
 namespace Unity.PolySpatial.XR.Input
 {
+    /// <summary>
+    /// Utility class for working with the Unity Input System.
+    /// </summary>
     public static class InputSystemUtility
     {
+        /// <summary>
+        /// Subscribes performed and canceled callbacks to an InputActionReference.
+        /// </summary>
+        /// <param name="reference">The input action reference to subscribe to.</param>
+        /// <param name="performed">The action to be called when the input action is made.</param>
+        /// <param name="canceled">The action to be called when the input action is canceled.</param>
         public static void Subscribe(InputActionReference reference, Action<InputAction.CallbackContext> performed = null, Action<InputAction.CallbackContext> canceled = null)
         {
             var action = GetInputAction(reference);
@@ -16,6 +25,12 @@ namespace Unity.PolySpatial.XR.Input
                 action.canceled += canceled;
         }
 
+        /// <summary>
+        /// Unsubscribes performed and canceled callbacks from an InputActionReference.
+        /// </summary>
+        /// <param name="reference">The input action reference to unsubscribe from.</param>
+        /// <param name="performed">The action to unsubscribe when the input action reference was performed.</param>
+        /// <param name="canceled">The action to unsubscribe when the input action reference was canceled.</param>
         public static void Unsubscribe(InputActionReference reference, Action<InputAction.CallbackContext> performed = null, Action<InputAction.CallbackContext> canceled = null)
         {
             var action = GetInputAction(reference);
@@ -26,6 +41,11 @@ namespace Unity.PolySpatial.XR.Input
                 action.canceled -= canceled;
         }
 
+        /// <summary>
+        /// Gets a reference to the InputAction from an InputActionReference.
+        /// </summary>
+        /// <param name="actionReference">The input action reference to obtain the input action from.</param>
+        /// <returns>The input action of the action reference.</returns>
         public static InputAction GetInputAction(InputActionReference actionReference)
         {
 #pragma warning disable IDE0031 // Use null propagation -- Do not use for UnityEngine.Object types
